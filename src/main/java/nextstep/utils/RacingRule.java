eb55974aca53a108a253fe45b0ce78d516ecdcea
rinjyu <rinjyu@naver.com>
2021-10-07 20:17:00 +0900

    feat: 자동차명과 횟수 유효성 검사 기능 추가
    - RacingRule 객체 추가
    - 유효성에 부합하지 않을 경우에 대한 RacingRuleException 추가

---
+++ src/main/java/nextstep/utils/RacingRule.java
@@ -0,0 +1,70 @@
+package nextstep.utils;
+
+import racinggame.RacingRuleException;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author Choi InJoo <rinjyu@naver.com>
+ * @version 1.0
+ * @since 1.0
+ */
+public class RacingRule {
+
+    private static final String RACING_CAR_DELIMITER = ",";
+
+    /**
+     * 데이터의 null or empty string 여부 확인
+     * @param data 데이터
+     * @return 데이터가 null 혹은 empty string 인지의 여부(true / false)
+     */
+    public static boolean isDataExists(String data) {
+        return !(data == null || data.trim().isEmpty());
+    }
+
+    /**
+     * 자동차명 내의 콤마(,) 포함 여부 확인
+     * @param racingCars 사용자가 입력한 자동차명
+     * @return 자동차명 내의 콤마(,)가 포함되어 있는 지의 여부(true / false)
+     */
+    public static boolean isRacingCarsNameContainsComma(String racingCars) {
+        return racingCars.contains(RACING_CAR_DELIMITER);
+    }
+
+    /**
+     * 자동차명의 글자수 확인
+     * @param racingCars 사용자가 입력한 자동차명
+     * @return 자동차명의 글자수가 1~5사이의 값인지의 여부(true / false)
+     */
+    public static boolean isEachRacingCarNameLength(String[] racingCars) {
+        for (String car: racingCars) {
+            if (!(car.length() > 0 && car.length() < 6)) return false;
+        }
+        return true;
+    }
+
+    /**
+     * 자동차명 유효성 확인
+     * @param racingCars 사용자가 입력한 자동차명
+     * @throws RacingRuleException 유효한 조건에 부합하지 않는 경우 발생
+     */
+    public static void isRacingCarsValidation(String racingCars) {
+        if (isDataExists(racingCars) && isRacingCarsNameContainsComma(racingCars) &&
+                isEachRacingCarNameLength(racingCars.split(RACING_CAR_DELIMITER))) {
+            return;
+        }
+        throw new RacingRuleException("[ERROR] 유효하지 않은 자동차명입니다.\n다시 입력해주세요.");
+    }
+
+    /**
+     * 횟수가 유효한 값인지의 여부 확인
+     * @param count 횟수
+     * @throws RacingRuleException 유효한 조건에 부합하지 않는 경우 발생
+     */
+    public static void isCountValidation(String count) {
+        if (Pattern.matches("^[0-9]*$", count) && Integer.parseInt(count) > 0) {
+            return;
+        }
+        throw new RacingRuleException("[ERROR] 숫자로 입력해주세요.");
+    }
+}
---
+++ src/main/java/racinggame/RacingRuleException.java
@@ -0,0 +1,15 @@
+package racinggame;
+
+/**
+ * @author Choi InJoo <rinjyu@naver.com>
+ * @version 1.0
+ * @since 1.0
+ */
+public class RacingRuleException extends IllegalArgumentException {
+
+    private static final long serialVersionUID = -5175815962570388596L;
+
+    public RacingRuleException(String message) {
+        super(message);
+    }
+}
